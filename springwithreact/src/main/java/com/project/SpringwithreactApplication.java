bfe26f9166f81cfc94f671c462bbb30dcc5197b0
naresh <dnareshmail@gmail.com>
2019-09-25 20:15:53 +0530

    first commit

---
+++ springwithreact/src/main/java/com/project/SpringwithreactApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithreactApplication.class, args);
+	}
+
+}
